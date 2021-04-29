50426fac918a304881c3335351828767d5452b98
siseko <s.nomavila@gmail.com>
2021-04-29 21:59:50 +0200

    adds more tests and to do list on read me

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 - Run `npm run test:watch` to run the tests in watch mode. This will re-run the tests for any code changes you make.
 - if you want to run just your bank file for some reason you can run `npm start`.
 - To debug you can use the **version control** tab on the left pane in vs code.
+
+### TODO list
+
+- Fix return message if user tries to deposit an amount less than or equal to zero. At the moment it says insufficient funds, this is not appropriate.
+
+```javascript
+deposit(accountNumber, -1000000);
+// TODO: fix message so it is meaningful
+```
--- bank.test.js
+++ bank.test.js
@@ -18,6 +18,11 @@ describe("Withdraw", () => {
     account.transactions = [];
   });
 
+  it("should not allow withdrawals of 0", () => {
+    const result = withdraw(accountNumber, 0);
+    expect(result).not.toBe("successful");
+  });
+
   it("should return insufficient funds if amount > balance", () => {
     const result = withdraw(accountNumber, 101);
     expect(result).toBe("insufficient funds");
@@ -43,5 +48,49 @@ describe("Withdraw", () => {
     const transactions = getAccount(accountNumber).transactions;
     expect(transactions.length).toBe(1);
     expect(transactions[0]).toEqual({ amount: -10, type: "withdrawal" });
+  });
+});
+
+describe("Deposit", () => {
+  const accountNumber = "123456789";
+  const invalidAccountNumber = "000000000";
+
+  beforeEach(() => {
+    const account = getAccount("123456789");
+    account.balance = 100;
+    account.transactions = [];
+  });
+
+  it("should not allow deposits of 0", () => {
+    const result = deposit(accountNumber, 0);
+    expect(result).not.toBe("successful");
+  });
+
+  it("should not allow a deposit of less than 0", () => {
+    const result = deposit(accountNumber, -1000000);
+    // TODO: fix message so it is meaningful
+    expect(result).toBe("insufficient funds");
+  });
+
+  it("should return success if amount > 0", () => {
+    const result = deposit(accountNumber, 10);
+    expect(result).toBe("successful");
+  });
+
+  it("should add to balance", () => {
+    deposit(accountNumber, 100);
+    expect(getAccount(accountNumber).balance).toBe(200);
+  });
+
+  it("should alert on invalid account number", () => {
+    const result = deposit(invalidAccountNumber, 10);
+    expect(result).toBe("the account number does not exist");
+  });
+
+  it("should add a transaction", () => {
+    deposit(accountNumber, 100);
+    const transactions = getAccount(accountNumber).transactions;
+    expect(transactions.length).toBe(1);
+    expect(transactions[0]).toEqual({ amount: 100, type: "deposit" });
   });
 });
