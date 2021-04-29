f70ccb40775144737efbde635fad2c9ff58e0cb0
siseko <s.nomavila@gmail.com>
2021-04-29 21:47:24 +0200

    adds some more tests

--- bank.test.js
+++ bank.test.js
@@ -3,34 +3,45 @@ const { withdraw, deposit, transfer, getAccount } = require("./bank");
 /**
  *     
  * accountNumber: '123456789', 100
-    accountNumber: '987654321', 200
-    accountNumber: '111111111', 500
-    accountNumber: '222222222', 900
-    accountNumber: '333333333', 50
+ accountNumber: '987654321', 200
+ accountNumber: '111111111', 500
+ accountNumber: '222222222', 900
+ accountNumber: '333333333', 50
  */
 describe("Withdraw", () => {
+  const accountNumber = "123456789";
+  const invalidAccountNumber = "000000000";
+
   beforeEach(() => {
-    getAccount("123456789").balance = 100;
+    const account = getAccount("123456789");
+    account.balance = 100;
+    account.transactions = [];
   });
 
   it("should return insufficient funds if amount > balance", () => {
-    const result = withdraw("123456789", 101);
+    const result = withdraw(accountNumber, 101);
     expect(result).toBe("insufficient funds");
   });
 
   it("should return success if amount < balance", () => {
-    const result = withdraw("123456789", 10);
+    const result = withdraw(accountNumber, 10);
     expect(result).toBe("successful");
   });
 
   it("should deduct balance", () => {
-    const accountNumber = "123456789";
     withdraw(accountNumber, 10);
     expect(getAccount(accountNumber).balance).toBe(90);
   });
 
   it("should alert on invalid account number", () => {
-    const result = withdraw("000000000", 10);
+    const result = withdraw(invalidAccountNumber, 10);
     expect(result).toBe("the account number does not exist");
+  });
+
+  it("should add a transaction", () => {
+    withdraw(accountNumber, 10);
+    const transactions = getAccount(accountNumber).transactions;
+    expect(transactions.length).toBe(1);
+    expect(transactions[0]).toEqual({ amount: -10, type: "withdrawal" });
   });
 });
