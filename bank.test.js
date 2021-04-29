6aeea1b39e8190a59cd98f97022e7c35a64571a2
siseko <s.nomavila@gmail.com>
2021-04-29 22:16:30 +0200

    concludes tests for the 3 main functions

--- bank.test.js
+++ bank.test.js
@@ -23,6 +23,11 @@ describe("Withdraw", () => {
     expect(result).not.toBe("successful");
   });
 
+  it("should not allow withdrawals of amount < 0", () => {
+    const result = withdraw(accountNumber, -10000);
+    expect(result).not.toBe("successful");
+  });
+
   it("should return insufficient funds if amount > balance", () => {
     const result = withdraw(accountNumber, 101);
     expect(result).toBe("insufficient funds");
@@ -92,5 +97,86 @@ describe("Deposit", () => {
     const transactions = getAccount(accountNumber).transactions;
     expect(transactions.length).toBe(1);
     expect(transactions[0]).toEqual({ amount: 100, type: "deposit" });
+  });
+});
+
+describe("Transfer", () => {
+  const fromAccountNumber = "123456789";
+  const toAccountNumber = "987654321";
+
+  beforeEach(() => {
+    const source = getAccount(fromAccountNumber);
+    source.balance = 100;
+    source.transactions = [];
+
+    const destination = getAccount(toAccountNumber);
+    destination.balance = 50;
+    destination.transactions = [];
+  });
+
+  it("should not allow transfers of 0", () => {
+    const result = transfer(fromAccountNumber, toAccountNumber, 0);
+    expect(result).not.toBe("successful");
+  });
+
+  it("should not allow transfers of amount < 0", () => {
+    const result = transfer(fromAccountNumber, toAccountNumber, -1000000);
+    expect(result).not.toBe("successful");
+  });
+
+  it("should alert on invalid from account", () => {
+    const result = transfer('00000000', toAccountNumber, 0);
+    expect(result).toBe("account number_00000000_is incorrect");
+  });
+
+  it("should alert on invalid to account", () => {
+    const result = transfer(fromAccountNumber, '00000000', 0);
+    expect(result).toBe("account number_00000000_is incorrect");
+  });
+
+  it("should return insufficient funds if amount > source.balance", () => {
+    const result = transfer(fromAccountNumber, toAccountNumber, 110);
+    expect(result).toBe("insufficient funds");
+  });
+
+  it("should return success if amount < source.balance", () => {
+    const result = transfer(fromAccountNumber, toAccountNumber, 99);
+    expect(result).toBe("successful");
+  });
+
+  it("should return success if amount = source.balance", () => {
+    const result = transfer(fromAccountNumber, toAccountNumber, 100);
+    expect(result).toBe("successful");
+  });
+
+  it("should return success if amount = source.balance", () => {
+    const result = transfer(fromAccountNumber, toAccountNumber, 100);
+    expect(result).toBe("successful");
+  });
+
+  it("should update source.balance", () => {
+    transfer(fromAccountNumber, toAccountNumber, 100);
+    const account = getAccount(fromAccountNumber);
+    expect(account.balance).toBe(0);
+  });
+
+  it("should update destination.balance", () => {
+    transfer(fromAccountNumber, toAccountNumber, 100);
+    const account = getAccount(toAccountNumber);
+    expect(account.balance).toBe(150);
+  });
+
+  it("should add transaction to source", () => {
+    transfer(fromAccountNumber, toAccountNumber, 100);
+    const account = getAccount(fromAccountNumber);
+    expect(account.transactions.length).toBe(1);
+    expect(account.transactions[0]).toEqual({ amount: -100, type: "transfer" });
+  });
+
+  it("should add transaction to destination", () => {
+    transfer(fromAccountNumber, toAccountNumber, 100);
+    const account = getAccount(toAccountNumber);
+    expect(account.transactions.length).toBe(1);
+    expect(account.transactions[0]).toEqual({ amount: 100, type: "transfer" });
   });
 });
