df234bfe9b8cf4ae0707b61be8bccd5a5d0a352f
Athi <athenkosi.s1912@gmail.com>
2021-05-05 02:02:29 +0200

    loops, loops and somemore loops

--- loops2.0.js
+++ loops2.0.js
@@ -1,22 +1,63 @@
 "use strict"
 
-let list = function(numbs){
- let counter = 0
- while(counter < numbs.length){
+let list = function (numbs) {
+  let counter = 0
+  while (counter < numbs.length) {
     numbs[counter] = 0
     counter = counter + 1
- }
-    return numbs
+  }
+  return numbs
 };
 
-let double = function(values){
- let pos = 0
- while(pos < values.length){
-     values[pos] = values[pos] * 2
-     pos = pos + 1
+let double = function (values) {
+  let pos = 0
+  while (pos < values.length) {
+    values[pos] = values[pos] * 2
+    pos = pos + 1
+  }
+  return values
+};
+
+let even = function (amounts) {
+  let pos = 0
+  let list = []
+  while (pos < amounts.length) {
+    if (amounts[pos] % 2 == 0) {
+      list.push(amounts[pos])
     }
-    return values
+    pos = pos + 1
+  }
+  return list
+};
+
+let average = function (numbers) {
+  let pos = 0
+  let oddLength = 0
+  let evenSum = 0
+  let oddTotal = 0
+  while (pos < numbers.length) {
+    if (numbers[pos] % 2 == 0) {
+      evenSum = evenSum + numbers[pos]
+    }
+    if (numbers[pos] % 2 == 1) {
+      oddLength = oddLength + 1
+      oddTotal = oddTotal + numbers[pos]
+    }
+    pos = pos + 1
+  }
+  return { evenSum: evenSum, oddAverage: oddTotal / oddLength }
+};
+
+let reverse = function(srebmun){
+   let pos = srebmun.length - 1
+   let list = []
+   while(pos >= 0){
+    list.push(srebmun[pos])
+    pos = pos - 1
+   }
+   return list
 };
 
 debugger
 
+console.log(reverse([1,2,3,4,5]))
